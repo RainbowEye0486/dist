4946a96e391952f38672341ae24dcb6f4bfd4f74
RainbowEye0486 <47811511+RainbowEye0486@users.noreply.github.com>
2019-04-28 13:29:55 +0800

    Initial commit

---
+++ script/min.js
@@ -0,0 +1,42 @@
+{
+
+//variable
+let count=0
+
+//funcntion
+/*
+function addfood(){
+    alert('dfklk;l')
+}
+*/
+$('#start_order').click(function(){
+
+    //debug
+    count++;
+    console.log('start!!boo ya')
+    console.log(count)
+})
+
+$('.menber_icon').click(function(){
+
+    //debug
+    console.log('link to menber')
+})
+
+$('.menu_icon').on
+
+
+$('#addbutton').click(function(){
+
+    $('#addbutton1').css({
+        color:'red',
+        width:'10px'
+    })
+})
+
+
+//event
+
+
+
+}
